821dc18d85d8272fc543397d54bda167236a712c
John Biddiscombe <biddisco@cscs.ch>
2021-10-19 10:39:27 +0200

    Fix to allow start pool script to work with new CDO based workflows

--- binaries/start-pool-manager.sh
+++ binaries/start-pool-manager.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # $1 log-directory
 # $2 stop file name
+# $3 optional : /path/to/real/CDO-enabled/pool-manager
+# $4 optional : pminfo pool manager info file name (in $log_dir) 
 
 pname="$1/$2"
 tname="$1/pool-manager-stopped"
@@ -12,8 +14,15 @@ if [[ -f "$pname" ]]; then
     rm -f "$pname"
 fi
 
-echo "$time   ./cdo-watcher.sh $pname $tname" >> ~/commands.txt
-/home/scitech/shared-data/binaries/cdo-watcher.sh "$pname" "$tname"
+if [ "$3" ]; then
+    command="$3 $1 $4"
+    echo "Starting CDO enabled pool manager : $command"
+    $command
+    echo "Done"
+else
+    echo "$time   ./cdo-watcher.sh $pname $tname" >> ~/commands.txt
+    /home/scitech/shared-data/binaries/cdo-watcher.sh "$pname" "$tname"
+fi
 
 time=$(date +"%H:%M:%S")
 echo "$time - pool-manager     STOP" >> ~/commands.txt
