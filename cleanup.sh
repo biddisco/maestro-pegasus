ada00d4cd12baec92b98bbb3e06864dad7a9017f
John Biddiscombe <biddisco@cscs.ch>
2021-10-22 21:17:10 +0200

    Fixes to workflow for CDO operation

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,7 @@
-rm -rf ~/commands.txt
+# kill off any pool manager instance still going
+touch ~/scratch/pool-manager.stop
+sleep 1
+rm -rf ~/commands.txt   
 rm -rf wf-scratch wf-output scitech
 find . -name \*.dot -delete
 find . -name \*.pdf -delete
@@ -10,4 +13,7 @@ rm -rf /home/scitech/shared-data/binaries/f*
 rm -rf /home/scitech/shared-data/binaries/trigger*
 rm -rf /home/scitech/shared-data/binaries/kill-pool-manager
 rm -rf /home/scitech/shared-data/scratch/*
+rm -rf /home/scitech/scratch/*
+rm -rf ./core*
+
 cp /home/scitech/shared-data/maestro-test/binaries/data/root-data.txt /home/scitech/scratch/
